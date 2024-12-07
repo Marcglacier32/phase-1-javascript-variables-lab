8b94a6eabce78665e6aa0d8970f8dcb3ed13cbfa
Mark Thiongo <mark.thiongo@student.moringaschool.com>
2024-12-07 16:46:09 +0300

    Updated index.js and package.json

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 // 1. Define companyName as a constant
 const companyName = "Scuber";
 
-// 2. Define mostProfitableNeighborhood using let
-let mostProfitableNeighborhood = "Chelsea";
+// 2. Define mostProfitableNeighborhood using constant
+const mostProfitableNeighborhood = "Chelsea";
 
 // 3. Define companyCeo using let
 let companyCeo = "Susan Smith";
