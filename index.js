d2546686de90cc7d030be54790666a5d5a1e5b62
Mark Thiongo <mark.thiongo@student.moringaschool.com>
2024-12-08 07:12:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// Code your solution in this file!
-// Code your solution in this file!
+// index.js
 
 // 1. Define companyName as a constant
 const companyName = "Scuber";
 
-// 2. Define mostProfitableNeighborhood using constant
-const mostProfitableNeighborhood = "Chelsea";
+// 2. Define mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = "Chelsea";
 
 // 3. Define companyCeo using let
 let companyCeo = "Susan Smith";
+
+// These variables are now globally accessible and can be tested by your test files.
